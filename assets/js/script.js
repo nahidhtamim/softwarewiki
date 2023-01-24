f885726aff64a5e6750c43df16b22dd29103a8d5
Md. Nahid Hasan Tamim <nahidhtamim@gmail.com>
2023-01-24 22:07:52 +0600

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 /*
 
-TemplateMo 559 Zay Shop
+softwarewiki 559 Zay Shop
 
-https://templatemo.com/tm-559-zay-shop
+https://softwarewiki.com/tm-559-zay-shop
 
 */
 
@@ -10,9 +10,9 @@ https://templatemo.com/tm-559-zay-shop
 $(document).ready(function() {
 
     // Accordion
-    var all_panels = $('.templatemo-accordion > li > ul').hide();
+    var all_panels = $('.softwarewiki-accordion > li > ul').hide();
 
-    $('.templatemo-accordion > li > a').click(function() {
+    $('.softwarewiki-accordion > li > a').click(function() {
         console.log('Hello world!');
         var target =  $(this).next();
         if(!target.hasClass('active')){
